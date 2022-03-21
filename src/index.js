897f7a3e95dbbff3f0f9bec94f79855b65d58454
Artur <sarnackiartur@gmail.com>
2022-03-21 18:13:10 +0100

    added styles

--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme } from '@mui/system';
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline/>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
