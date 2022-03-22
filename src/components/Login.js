1ff333b2460af8acf10bc20d356905c570168bc8
Artur <sarnackiartur@gmail.com>
2022-03-22 17:29:11 +0100

    added some components, implement db to project -.-

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,48 @@
-import { Button, TextField, Box, CardContent, Card, Typography } from "@mui/material";
-import React from "react";
+import {
+  Button,
+  TextField,
+  Box,
+  CardContent,
+  Card,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 import Center from "./Center";
+import useForm from "../hooks/useForm";
+import useStateContext from "../hooks/useStateContext";
+import { createEndpoint, ENDPOINTS } from "../api";
+
+
+const getFreshModel = () => ({
+  name: "",
+  email: "",
+});
 function Login() {
+  const { values, setValues, errors, setErrors, handleInputChange } =
+    useForm(getFreshModel);
+
+  const {context, setContext} = useStateContext()
+
+  const validate = () => {
+    let temp = {};
+    temp.email = (/\S+@\S+\.\S+/).test(values.email) ? "" : "Email is not valid."
+    temp.name = values.name !== "" ? "" : "This field is required.";
+    setErrors(temp);
+    return Object.values(temp).every((x) => x === "");
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    if (validate()) createEndpoint(ENDPOINTS.participant).post(values).then(res => {setContext({participantId:res.data.participantId})}).catch(e => console.log(e))
+  };
+
   return (
     <Center>
       <Card sx={{ width: 400 }}>
-        <CardContent sx={{textAlign:'center'}}>
-          <Typography variant="h3" sx={{my:3}}>
-          Quiz App</Typography>
+        <CardContent sx={{ textAlign: "center" }}>
+          <Typography variant="h3" sx={{ my: 3 }}>
+            Quiz App
+          </Typography>
           <Box
             sx={{
               "& .MuiTextField-root": {
@@ -16,14 +51,23 @@ function Login() {
               },
             }}
           >
-            <form noValidate>
+            <form noValidate onSubmit={login}>
               <TextField
                 label="Email"
                 name="email"
+                value={values.email}
                 variant="outlined"
-                autoComplete="off"
+                {...(errors.email && { error: true, helperText: errors.email })}
+                onChange={handleInputChange}
               />
-              <TextField label="Name" name="name" variant="outlined" />
+              <TextField
+                label="Name"
+                name="name"
+                variant="outlined"
+                value={values.name}
+                {...(errors.name && { error: true, helperText: errors.name })}
+                onChange={handleInputChange}
+              />
               <Button
                 type="submit"
                 variant="contained"
